c3044b1b52aa5b17e24a391076d046d2ec6d6b40
debija <42231132+JohanAndresen@users.noreply.github.com>
2023-05-16 12:56:23 +0200

    the loop over files evolves

--- src/wrangling_rebels.py
+++ src/wrangling_rebels.py
@@ -3,7 +3,6 @@
 import glob 
 import pandas as pd
 import networkx as nx
-# import numpy as np
 import bin.rebel_decode as rd
 
 
@@ -23,24 +22,29 @@ public_dfs = [] # list of df's for concatenation at loop end
 
 for sample_no, filename in enumerate(public_list, start=1):
 
-    print('sample number: {} \n file path {}'.format((sample_no), (filename)))
+    print('Sample number: {} \nFile path: "{}"'.format((sample_no), (filename)))
+    
+    # extract pandas objects
     p_info = rd.parse_public_data(filename)
+    COT=p_info.get_cot() # df of messenger's cotraveller at t
     rebs_df=p_info.get_rebs_df()
-
-    # make ships unique across samples
-    rebs_df['ship_sample'] = rebs_df.apply(lambda df_x: pd.isna if pd.isna(df_x['ship']) else df_x['sample']+'_'+str(df_x['ship']),axis=1)
 
     # estimate ships
     relations = nx.from_pandas_edgelist(COT, source='messenger', target='cotraveller')
     ships = {rebel: shipnumber for shipnumber, ship in enumerate(nx.connected_components(relations), start=1) for rebel in ship}
     rebs_df['ship'] = rebs_df['messenger'].map(ships)
-    
     print("How many rebels' ships do we fail to identify?\n", int(rebs_df['ship'].isna().sum()/1000))
     print('How many identified groups of rebels aka ships are there?\n',rebs_df.ship.nunique())
     # print('What are the groups?',rebs_df.ship.value_counts())
     
-    rebs_df['ship_sample'] = rebs_df.apply(lambda df_x: pd.isna if pd.isna(df_x['ship']) else str(df_x['sample'])+'_'+str(int(df_x['ship'])),axis=1)
+    ## make ships unique across samples to imply/convey no cross-sample information
+    rebs_df['ship_sample'] = rebs_df.apply(lambda df_x: pd.isna if pd.isna(df_x['ship']) else str(int(df_x['ship']))+'_'+str(df_x['sample']),axis=1)
 
+    # inspect
+    print(rebs_df.info())
+    print('What type of leaks are most common? (are they?)')
+    print(rebs_df.value_counts('msg_type'))
+    print(rebs_df.groupby(['msg_type'])['messenger'].nunique())
 
     public_dfs.append(rebs_df)
 
