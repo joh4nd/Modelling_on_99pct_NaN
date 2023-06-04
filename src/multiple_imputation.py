8a06b22cdad27b008bf9236b95b54c59390e5fbb
debija <42231132+JohanAndresen@users.noreply.github.com>
2023-06-04 16:10:10 +0200

    moved some more files before Amelia

--- src/multiple_imputation.py
+++ src/multiple_imputation.py
@@ -1,2 +1,15 @@
 # multiple imputation
 
+from wrangling_rebels_v2 import wr_2
+
+dfs = wr_2()
+
+# load Amelia for handling missing data
+from rpy2.robjects.packages import importr
+from rpy2.robjects import r
+from rpy2.robjects import pandas2ri 
+
+
+Amelia = importr('Amelia')
+dir(Amelia)
+Amelia.
