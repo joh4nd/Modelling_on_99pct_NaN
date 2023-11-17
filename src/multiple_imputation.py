7fe0a10b4a06904ca85ce5848d8701e074750b8c
debija <42231132+JohanAndresen@users.noreply.github.com>
2023-11-17 10:01:48 +0100

    started building runner

--- src/multiple_imputation.py
+++ src/multiple_imputation.py
@@ -1,31 +1,9 @@
-# multiple imputation
+# multiple imputation version 2
 
 from wrangling_rebels import wr
 import pandas as pd
 import numpy as np
 
-
-
-# load data
-dfs = wr() # understand dfs, see: describe_dfs.py
-dfs_MI = dfs[dfs.columns[~dfs.columns.isin(['index','messengerId_truth','shipNo','shipId','messenger','nearestStarId','nearestStar_x','nearestStar_y','nearestStar_z','nearestStar_nNeigh','msg_type'])]].copy() # deselect variables expected not to be useful by MI in presence of similar variables with more unique values better preventing falsely calculated identities. shipId, nearestStarId/x/y/z/nNeigh, msg_type are useful for ML, not MI.
-# NOTE 1) should we drop all rows being NaN in msg_type (perhaps for ML)?
-
-
-
-# convert categorical into numeric, required by MI. Amelia understands nominal variables
-cols_to_cat = ['sampleNo','messengerId','shipId_truth','nearestStarId_truth']
-dfs_MI[cols_to_cat] = dfs_MI[cols_to_cat].astype('category').apply(lambda x_col: x_col.cat.codes).replace(-1,np.nan)
-print('cat nulls before conversion:\n\n', dfs[cols_to_cat].isna().sum(),'\n\n','cat nulls after conversion:\n\n',dfs_MI[cols_to_cat].isna().sum(),sep="")
-del dfs
-
-# save to csv for R processes (before handled through rpy2)
-dfs_MI.to_csv('../data/dfs_MI.csv',index_label='index') #, index=False)
-print(dfs_MI.columns)
-
-
-
-############################################################3
 
 # setup Amelia in python
 from rpy2.robjects.packages import importr # guidelines: https://github.com/joh4nd/amelia-rpy2
@@ -40,71 +18,48 @@ from rpy2.robjects import pandas2ri
 # Amelia = importr('Amelia')
 # dir(Amelia)
 # # Amelia.
-
-
-
-class MultipleImputer:
-
-    def __init__(self, imputation_method="Amelia"):
-        """imputation_method can be "Amelia" or "mice"."""
-        self.imputation_method = imputation_method
-
-    def impute_data(self, preprocessed_df):
-        if self.imputation_method == "Amelia":
-            return self.amelia_imputation(preprocessed_df)
-        elif self.imputation_method == "mice":
-            return self.mice_imputation(preprocessed_df)
-        else:
-            raise ValueError(f"Invalid imputation method: {self.imputation_method}")
     
-    def amelia_imputation(self, preprocessed_df):
-        """Amelia runs through rpy2"""
 
-        r_dataframe = pandas2ri.py2ri(preprocessed_df)
+def impute(imputation_method="Amelia", df=df):
 
-        r_script = """
-        
-        pacman::p_load("Amelia", "dplyr")
+    r_dataframe = pandas2ri.py2ri(preprocessed_df)
 
-        # convert Pandas dataframe to R dataframe through rpy2
-        rebels <- read.csv("./repos/opg_RR/data/dfs_MI.csv")
+    r_script = """
+    
+    pacman::p_load("Amelia", "dplyr")
 
-        # run
-        MI <- amelia(x = rebels1,
-            m = 5,
-            p2s = 1,
-            idvars = c("index","sampleNo"),
-            noms = c("messengerId","nearestStarId_truth"),
-            ords = "atDest_moving",
-            ts = "t",
-            cs = "shipId_truth",
-            empri = 0.05 * nrow(rebels1),
-            polytime = 2,
-            intercs = TRUE,
-            bounds = matrix(c(11,0,1000, 12,0,1000, 13,0,1000), nrow = 3, ncol = 3, byrow = TRUE),
-            parallel = 'multicore',
-            ncpus = 8) 
+    # convert Pandas dataframe to R dataframe through rpy2
+    rebels <- read.csv("./repos/opg_RR/data/dfs_MI.csv")
 
-        # diagnostics
+    # run
+    MI <- amelia(x = rebels1,
+        m = 5,
+        p2s = 1,
+        idvars = c("index","sampleNo"),
+        noms = c("messengerId","nearestStarId_truth"),
+        ords = "atDest_moving",
+        ts = "t",
+        cs = "shipId_truth",
+        empri = 0.05 * nrow(rebels1),
+        polytime = 2,
+        intercs = TRUE,
+        bounds = matrix(c(11,0,1000, 12,0,1000, 13,0,1000), nrow = 3, ncol = 3, byrow = TRUE),
+        parallel = 'multicore',
+        ncpus = 8) 
 
-        # simulation to draw samples of m imputated datasets, to be combined in one final dataset
-        # NOTE this may alternatively be done using numpy random functions
-        
+    # diagnostics
 
-        """
-        
-        imputed_data = robjects.r(r_script)
+    # simulation to draw samples of m imputated datasets, to be combined in one final dataset
+    # NOTE this may alternatively be done using numpy random functions
+    
 
-        imputed_df = pandas2ri.ri2py_dataframe(imputed_data)
+    """
+    
+    imputed_data = robjects.r(r_script)
 
-        return imputed_data
+    imputed_df = pandas2ri.ri2py_dataframe(imputed_data)
 
-    def mice_imputation(self, preprocessed_df):
-        """placeholder for mice implementation"""
-        # imp = mice(preprocessed_df)
-        # imputed_data = imp.complete()
-        # return imputed_data
-        pass
+    return imputed_data
 
 
 
@@ -113,9 +68,13 @@ class MultipleImputer:
 
 
 
-# use data pipeline class to concatenate
+
+
+# concatenate
 
 # # split data into training and test
 # test_samples = random.sample(df['Sample'].unique().tolist(), 2) # draw 2 random  samples
 # train_data = df[~df['Sample'].isin(test_samples)]
 # test_data = df[df['Sample'].isin(test_samples)]
+
+
--- src/wrangling_rebels.py
+++ src/wrangling_rebels.py
@@ -1,147 +1,146 @@
 # wrangling rebels version 3
 # adds calculation of the truest nearest star
 
-import glob 
+
 import re
 import pandas as pd
 import numpy as np
 import networkx as nx
 import bin.rebel_decode as rd
+from sample_set import path_to_set
 from sklearn.neighbors import NearestNeighbors
 
-
-######################################
-#########   all 10 samples   #########
-######################################
-
-def wr(directory_to_files="../data/*.txt"):
+def wrangle(sample_set=f'0001'):
     """
     Parses the rebel files using rebel_decode.py.
 
     Parameters:
-    directory_to_files (str): the path to the rebel .txt-files.
+    sample_set (str): the path to the rebel .txt-files.
 
     Returns:
     pandas.DataFrame: The wrangled, concatenated dataframes ready for multiple imputation.
-
     """
 
-    files = glob.glob(directory_to_files) # list all files, some for rebel_decode
-    sample_set = sorted({re.search(r"(\d{4})_", filetype).group(1) for filetype in files if re.search(r"(\d{2})_", filetype)}) # make time-series iterator
-    print('Number of time-series: ', len(sample_set))
-    dfs = [] # list of df's for concatenation at loop end
-
-    for sample_no in sample_set:
+    # extract objects
+    p_info = rd.parse_public_data(f"../data/{sample_set}_public.txt")
+    truth = rd.parse_truth_data(f"../data/{sample_set}_truth.txt")
         
-        print(f"Processing sample: {sample_no}")
+    # estimate ships from COT and rebs_df
+    rebs_df=p_info.get_rebs_df()
+    COT=p_info.get_cot() # df of messenger's cotraveller at t
 
-        # extract pandas objects
-        p_info = rd.parse_public_data(f"../data/{sample_no}_public.txt")
-        truth = rd.parse_truth_data(f"../data/{sample_no}_truth.txt")
+    relations = nx.from_pandas_edgelist(COT, source='messenger', target='cotraveller')
+    ships = {rebel: shipnumber for shipnumber, ship in enumerate(nx.connected_components(relations), start=1) for rebel in ship}
+    rebs_df['ship'] = rebs_df['messenger'].map(ships).astype(float) #.astype(pd.Int16Dtype()). NOTE: Pandas 2.0 allows int to handle NaN 
+
+    print("\n How many rebels' ships do we fail to identify?\n", int(rebs_df['ship'].isna().sum()/1000)) # t=1000
+    
+    rebs_df['shipId'] = rebs_df.apply(lambda df_x: np.nan if pd.isna(df_x['ship']) else str(int(df_x['ship']))+df_x['sampleNo'],axis=1) # use sample identifer from df I added to rebel_decode to ensure each ship has a unique ID. Makes ships unique across samples to imply no cross-sample information. NOTE: Pandas 2.0 allows int to handle NaN. see perhaps pd.na
+    
+    #region: get TRUTH for multiple imputation
+
+    ## messages (LOC,NEA,COT leaks in one df)
+    messages = truth.get_messages()
+    rebel_id = messages[['id','name','shipid']].drop_duplicates(subset=['id','name','shipid']).sort_values(by='name')
+    rebs_df = rebs_df.merge(rebel_id, how='left', left_on='messenger', right_on='name').drop(labels='name', axis=1)
+
+    if rebs_df['shipid'].isna().sum() > 0:
+        print('\nrebels lacking shipid: {}\n'.format(rebs_df['shipid'].isna().sum()))
+
+    ## ship movements
+    ship_movements = truth.get_moves()
+    ship_movements.columns=['t', 'x_truth', 'y_truth', 'z_truth','shipid','at_dest']
+    ship_movements['at_dest'] = ship_movements['at_dest'].map({'true': 1, 'false': 0})
+    rebs_df = rebs_df.merge(ship_movements, how='left', on=['t','shipid'], suffixes=('', '__y'))
+    rebs_df.drop(rebs_df.filter(regex='^.*(__y)').columns, axis=1, inplace=True)
+
+    ## get NEA of leaker and impute to ship members USING TRUTH ID
+    NEA=p_info.get_nea() # df of messenger's closest star
+    NEA.columns=['messenger', 't', 'nearestStar']
+    NEA = NEA.merge(rebel_id, how='left', left_on='messenger', right_on='name').drop(labels=['id','name'], axis=1)
+
+    if NEA['shipid'].isna().sum() < 1:
+        rebs_df = pd.merge(rebs_df,NEA[['t','shipid','nearestStar']], how='left', on=['shipid','t'], suffixes=('', '__y'))
+
+        if rebs_df.duplicated().astype(int).sum() > 0:
+            print('Duplicates after join NEA: '), print(rebs_df.duplicated().astype(int).sum())
+
+            rebs_df.drop_duplicates(inplace=True)
+
+    else:
+        print('\n\nNEA leakers with unidentified ships: {}\n'.format(NEA['shipid'].isna().sum()))
+
+        rebs_df = pd.merge(rebs_df,NEA[['messenger','t','nearestStar']], how='left',on=['messenger','t'], suffixes=('', '__y'))
+
+        print('\nnearest stars first time: {}'.format(rebs_df['nearestStar'].notna().sum()))
+
+        NEA2 = NEA.loc[NEA['shipid'].notna()].copy()
+        
+        if NEA2[['shipid','t','nearestStar']].duplicated().sum() > 0:
             
-        # estimate ships from COT and rebs_df
-        rebs_df=p_info.get_rebs_df()
-        COT=p_info.get_cot() # df of messenger's cotraveller at t
+            print('\nshipmembers duplicate NEA leaks: {}'.format(NEA2[['shipid','t','nearestStar']].duplicated().sum()))
+            NEA2 = NEA2[['shipid','t','nearestStar']].drop_duplicates().sort_values(by=['shipid','t'])
 
-        relations = nx.from_pandas_edgelist(COT, source='messenger', target='cotraveller')
-        ships = {rebel: shipnumber for shipnumber, ship in enumerate(nx.connected_components(relations), start=1) for rebel in ship}
-        rebs_df['ship'] = rebs_df['messenger'].map(ships).astype(float) #.astype(pd.Int16Dtype()). NOTE: Pandas 2.0 allows int to handle NaN 
-
-        print("\n How many rebels' ships do we fail to identify?\n", int(rebs_df['ship'].isna().sum()/1000)) # t=1000
+        rebs_df = rebs_df.combine_first(rebs_df.drop('nearestStar',axis=1).merge(NEA2, how='left',on=['shipid','t']))
         
-        rebs_df['shipId'] = rebs_df.apply(lambda df_x: np.nan if pd.isna(df_x['ship']) else str(int(df_x['ship']))+df_x['sampleNo'],axis=1) # use sample identifer from df I added to rebel_decode to ensure each ship has a unique ID. Makes ships unique across samples to imply no cross-sample information. NOTE: Pandas 2.0 allows int to handle NaN. see perhaps pd.na
-        
-        #region: get TRUTH for multiple imputation
+        print('nearest stars second time: {}\n'.format(rebs_df['nearestStar'].notna().sum()))
 
-        ## messages (LOC,NEA,COT leaks in one df)
-        messages = truth.get_messages()
-        rebel_id = messages[['id','name','shipid']].drop_duplicates(subset=['id','name','shipid']).sort_values(by='name')
-        rebs_df = rebs_df.merge(rebel_id, how='left', left_on='messenger', right_on='name').drop(labels='name', axis=1)
+    ## star coordinates
+    star_coords = truth.get_stars()
+    star_coords.columns=['nearestStar_x', 'nearestStar_y', 'nearestStar_z', 'nearestStar_nNeigh','starid']
+    rebs_df = rebs_df.merge(star_coords, how='left', left_on='nearestStar', right_on='starid').drop(labels='starid', axis=1) # append only to leaks
 
-        if rebs_df['shipid'].isna().sum() > 0:
-            print('\nrebels lacking shipid: {}\n'.format(rebs_df['shipid'].isna().sum()))
+    ## calculate true nearest star (fill leaked nearest star or substitute during MI)
+    ship_coordinates = ship_movements[['x_truth', 'y_truth', 'z_truth']].values
+    star_coordinates = star_coords[['nearestStar_x', 'nearestStar_y', 'nearestStar_z']].values
+    distance_matrix = np.linalg.norm(ship_coordinates[:, np.newaxis] - star_coordinates, axis=2) # Euclidian distances between N stars x M ship locations
+    ship_moves_nearest_star = np.argmin(distance_matrix, axis=1) # index of nearest star for each ship movement e.g. ship movement 0; star 155
+    ship_movements[['nearestStarId_truth','nearestStar_truth_x','nearestStar_truth_y','nearestStar_truth_z']] = star_coords.loc[ship_moves_nearest_star, ['starid','nearestStar_x','nearestStar_y','nearestStar_z']].values # nearest star and coordinates for each ship
 
-        ## ship movements
-        ship_movements = truth.get_moves()
-        ship_movements.columns=['t', 'x_truth', 'y_truth', 'z_truth','shipid','at_dest']
-        ship_movements['at_dest'] = ship_movements['at_dest'].map({'true': 1, 'false': 0})
-        rebs_df = rebs_df.merge(ship_movements, how='left', on=['t','shipid'], suffixes=('', '__y'))
-        rebs_df.drop(rebs_df.filter(regex='^.*(__y)').columns, axis=1, inplace=True)
+    ## calculate true nearest neighbors of nearest star
+    neighbor_stars = NearestNeighbors(radius=50) # neighbors within radius ~½% of 3d space
+    neighbor_stars.fit(star_coordinates)
+    all_neighbors = neighbor_stars.radius_neighbors(star_coordinates, return_distance=False) # list neighbors of each star
+    all_neighbors = [np.array(neighbors) for neighbors in all_neighbors] # to array
+    num_neighbors = [len(neighbors) -1 for neighbors in all_neighbors] # Calculate the number of neighbors for each star, not counting each star as its own neighbor
+    star_coords['nNeigh_truth'] = num_neighbors
+    nNeigh_corr = star_coords['nearestStar_nNeigh'].corr(star_coords['nNeigh_truth']) # correlation between predicted and given nNeigh
+    print(f"Correlation between given and predicted nNeigh: {nNeigh_corr:.2f}\n")
+    ship_movements['nearestStar_nNeigh_truth'] = star_coords.loc[ship_moves_nearest_star, 'nNeigh_truth'].values
 
-        ## get NEA of leaker and impute to ship members USING TRUTH ID
-        NEA=p_info.get_nea() # df of messenger's closest star
-        NEA.columns=['messenger', 't', 'nearestStar']
-        NEA = NEA.merge(rebel_id, how='left', left_on='messenger', right_on='name').drop(labels=['id','name'], axis=1)
+    ## add nearest star id, position, and nNeigh to df
+    rebs_df = rebs_df.merge(ship_movements[['t','shipid','nearestStarId_truth','nearestStar_truth_x', 'nearestStar_truth_y', 'nearestStar_truth_z','nearestStar_nNeigh_truth']], how='left', on=['t','shipid'], suffixes=('', '__y'))
 
-        if NEA['shipid'].isna().sum() < 1:
-            rebs_df = pd.merge(rebs_df,NEA[['t','shipid','nearestStar']], how='left', on=['shipid','t'], suffixes=('', '__y'))
+    #endregion
 
-            if rebs_df.duplicated().astype(int).sum() > 0:
-                print('Duplicates after join NEA: '), print(rebs_df.duplicated().astype(int).sum())
+    # get LOC of leaker and impute to ship members (may cause bias if rebels vary systematically in the noise they add to the coordinate)
+    LOC=p_info.get_loc() # df of messenger's location
+    rebs_df = pd.merge(rebs_df,LOC[['t','messenger','x','y','z']], how='left',on=['messenger','t'], suffixes=('', '__y'))
+    # WONTDO: manually impute LOC to shipmembers because members at t leak slightly different positions i.e. "noisy leaks". Multiple imputation most accurately derives accurate values.
 
-                rebs_df.drop_duplicates(inplace=True)
+    rebs_df.drop(rebs_df.filter(regex='^.*(__x|__y)').columns, axis=1, inplace=True)
 
-        else:
-            print('\n\nNEA leakers with unidentified ships: {}\n'.format(NEA['shipid'].isna().sum()))
-
-            rebs_df = pd.merge(rebs_df,NEA[['messenger','t','nearestStar']], how='left',on=['messenger','t'], suffixes=('', '__y'))
-
-            print('\nnearest stars first time: {}'.format(rebs_df['nearestStar'].notna().sum()))
-
-            NEA2 = NEA.loc[NEA['shipid'].notna()].copy()
-            
-            if NEA2[['shipid','t','nearestStar']].duplicated().sum() > 0:
-                
-                print('\nshipmembers duplicate NEA leaks: {}'.format(NEA2[['shipid','t','nearestStar']].duplicated().sum()))
-                NEA2 = NEA2[['shipid','t','nearestStar']].drop_duplicates().sort_values(by=['shipid','t'])
-
-            rebs_df = rebs_df.combine_first(rebs_df.drop('nearestStar',axis=1).merge(NEA2, how='left',on=['shipid','t']))
-            
-            print('nearest stars second time: {}\n'.format(rebs_df['nearestStar'].notna().sum()))
-
-        ## star coordinates
-        star_coords = truth.get_stars()
-        star_coords.columns=['nearestStar_x', 'nearestStar_y', 'nearestStar_z', 'nearestStar_nNeigh','starid']
-        rebs_df = rebs_df.merge(star_coords, how='left', left_on='nearestStar', right_on='starid').drop(labels='starid', axis=1) # append only to leaks
-
-        ## calculate true nearest star (fill leaked nearest star or substitute during MI)
-        ship_coordinates = ship_movements[['x_truth', 'y_truth', 'z_truth']].values
-        star_coordinates = star_coords[['nearestStar_x', 'nearestStar_y', 'nearestStar_z']].values
-        distance_matrix = np.linalg.norm(ship_coordinates[:, np.newaxis] - star_coordinates, axis=2) # Euclidian distances between N stars x M ship locations
-        ship_moves_nearest_star = np.argmin(distance_matrix, axis=1) # index of nearest star for each ship movement e.g. ship movement 0; star 155
-        ship_movements[['nearestStarId_truth','nearestStar_truth_x','nearestStar_truth_y','nearestStar_truth_z']] = star_coords.loc[ship_moves_nearest_star, ['starid','nearestStar_x','nearestStar_y','nearestStar_z']].values # nearest star and coordinates for each ship
-
-        ## calculate true nearest neighbors of nearest star
-        neighbor_stars = NearestNeighbors(radius=50) # neighbors within radius ~½% of 3d space
-        neighbor_stars.fit(star_coordinates)
-        all_neighbors = neighbor_stars.radius_neighbors(star_coordinates, return_distance=False) # list neighbors of each star
-        all_neighbors = [np.array(neighbors) for neighbors in all_neighbors] # to array
-        num_neighbors = [len(neighbors) -1 for neighbors in all_neighbors] # Calculate the number of neighbors for each star, not counting each star as its own neighbor
-        star_coords['nNeigh_truth'] = num_neighbors
-        nNeigh_corr = star_coords['nearestStar_nNeigh'].corr(star_coords['nNeigh_truth']) # correlation between predicted and given nNeigh
-        print(f"Correlation between given and predicted nNeigh: {nNeigh_corr:.2f}\n")
-        ship_movements['nearestStar_nNeigh_truth'] = star_coords.loc[ship_moves_nearest_star, 'nNeigh_truth'].values
-
-        ## add nearest star id, position, and nNeigh to df
-        rebs_df = rebs_df.merge(ship_movements[['t','shipid','nearestStarId_truth','nearestStar_truth_x', 'nearestStar_truth_y', 'nearestStar_truth_z','nearestStar_nNeigh_truth']], how='left', on=['t','shipid'], suffixes=('', '__y'))
-
-        #endregion
-
-        # get LOC of leaker and impute to ship members (may cause bias if rebels vary systematically in the noise they add to the coordinate)
-        LOC=p_info.get_loc() # df of messenger's location
-        rebs_df = pd.merge(rebs_df,LOC[['t','messenger','x','y','z']], how='left',on=['messenger','t'], suffixes=('', '__y'))
-        # WONTDO: manually impute LOC to shipmembers because members at t leak slightly different positions i.e. "noisy leaks". Multiple imputation most accurately derives accurate values.
-
-        rebs_df.drop(rebs_df.filter(regex='^.*(__x|__y)').columns, axis=1, inplace=True)
-
-        # rearrange and relabel df cols
-        rebs_df.rename(columns={'ship': 'shipNo', 'nearestStar':'nearestStarId','id': 'messengerId_truth', 'shipid': 'shipId_truth','at_dest':'atDest_moving'},inplace=True)
-        rebs_df = rebs_df[['sampleNo','messengerId','messengerId_truth','shipNo','shipId','shipId_truth','messenger','t','msg_type','nearestStarId','nearestStar_x','nearestStar_y','nearestStar_z','nearestStar_nNeigh','nearestStarId_truth','nearestStar_truth_x', 'nearestStar_truth_y', 'nearestStar_truth_z','nearestStar_nNeigh_truth','x','y','z','x_truth','y_truth','z_truth','atDest_moving']] # drop msg_content
+    # rearrange and relabel df cols
+    rebs_df.rename(columns={'ship': 'shipNo', 'nearestStar':'nearestStarId','id': 'messengerId_truth', 'shipid': 'shipId_truth','at_dest':'atDest_moving'},inplace=True)
+    rebs_df = rebs_df[['sampleNo','messengerId','messengerId_truth','shipNo','shipId','shipId_truth','messenger','t','msg_type','nearestStarId','nearestStar_x','nearestStar_y','nearestStar_z','nearestStar_nNeigh','nearestStarId_truth','nearestStar_truth_x', 'nearestStar_truth_y', 'nearestStar_truth_z','nearestStar_nNeigh_truth','x','y','z','x_truth','y_truth','z_truth','atDest_moving']] # drop msg_content
         
 
-        dfs.append(rebs_df)
 
-    dfs = pd.concat(dfs).reset_index().drop('index', axis=1)
-    dfs.info()
+    # load data
+    # dfs = wr() # understand dfs, see: describe_dfs.py
+    dfs_MI = rebs_df[rebs_df.columns[~rebs_df.columns.isin(['index','messengerId_truth','shipNo','shipId','messenger','nearestStarId','nearestStar_x','nearestStar_y','nearestStar_z','nearestStar_nNeigh','msg_type'])]].copy() # deselect variables expected not to be useful by MI in presence of similar variables with more unique values better preventing falsely calculated identities. shipId, nearestStarId/x/y/z/nNeigh, msg_type are useful for ML, not MI.
+    # NOTE 1) should we drop all rows being NaN in msg_type (perhaps for ML)?
 
-    return dfs
+
+
+    # convert categorical into numeric, required by MI. Amelia understands nominal variables
+    cols_to_cat = ['sampleNo','messengerId','shipId_truth','nearestStarId_truth']
+    rebs_df[cols_to_cat] = rebs_df[cols_to_cat].astype('category').apply(lambda x_col: x_col.cat.codes).replace(-1,np.nan)
+    print('cat nulls before conversion:\n\n', rebs_df[cols_to_cat].isna().sum(),'\n\n','cat nulls after conversion:\n\n',rebs_df[cols_to_cat].isna().sum(),sep="")
+    del rebs_df
+
+    # save to csv for R processes (before handled through rpy2)
+    dfs_MI.to_csv('../data/dfs_MI.csv',index_label='index') #, index=False)
+    print(dfs_MI.columns)
+
+    return dfs_MI
